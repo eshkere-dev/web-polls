e20703f10606dd4f2617ff2012a732e42bab0d48
4lgo1328 <ekcnert1337@gmail.com>
2024-11-09 17:28:17 +0300

    bobr kurwa

--- auth/routes.py
+++ auth/routes.py
@@ -1,7 +1,7 @@
 from flask import Blueprint, render_template, redirect, url_for, request, flash
 from flask_login import login_user, login_required, logout_user, current_user
 from .models import User
-
+from src.encoder import *
 
 auth_bp = Blueprint('auth_bp', __name__)
 
@@ -10,18 +10,20 @@ users = {
     'user2': {'password': 'password2'}
 }
 
+
 @auth_bp.route('/login', methods=['GET', 'POST'])
 def login():
     if request.method == 'POST':
         username = request.form['username']
         password = request.form['password']
 
+        user = User.query.filter_by(username=username).first()
+
         # Check if the user exists and password is correct
-        if username in users and users[username]['password'] == password:
-            user = User(username)
-            login_user(user)
+        if user and verify_password_bcrypt(user.password, password):  # Check if password matches
+            login_user(user)  # Log the user in
             flash('Login successful!', 'success')
-            return redirect(url_for('home'))
+            return redirect(url_for('home'))  # Redirect to the home page
         else:
             flash('Invalid username or password', 'danger')
 
